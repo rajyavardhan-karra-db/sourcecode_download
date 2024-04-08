5307a6863cc235c1158629d2ae1f30353b815a4c
rajyavardhan-karra-db <134555428+rajyavardhan-karra-db@users.noreply.github.com>
2024-04-08 07:18:07 -0400

    web upload db

---
+++ SourceCode.java
@@ -0,0 +1,26 @@
+(public|protected|private)?\s*(class|interface)\s\w+(\s(implements|extends)\s(\w+,\s?)?\w+)*(\s\{)?
+perl
+*.bat,*.cxx,*.asm,*.asp,*.java,*.cpp,*.c,*.h,*.c++,*.pl,*.cob,*.cbl,
+*.for,*.bas,*.cls,*.frm,*.vbp,*.vb,*.cfg,*.props,*.configuration
+Attribute\sVB_Name
+#include\s+(<|")[\w.]+(>|")
+options.addParamValue\(\'[\w'-]
+\{\s*.+\;\s*.+\;\s*.+\;\s*.+\;\s*.+\;
+commandwindow	(	3
+getpref
+setpref
+addpref
+rmpref
+ispref
+mathlab.io.saveVariablesToScript
+classdef
+isequal
+isobject
+plot(
+xlabel
+ylabel)
+
+import\s(\w+\.)+(\w+|\*);
+[#][!]
+\$[a-zA-Z_]\w+
+\(\s*.+\;\s*.+\;\s*.+\;\s*.+\;\s*.+\;
